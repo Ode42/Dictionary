32ebb4ded7fa8d39a3a41164421bfe136bfa8ddc
Ode42 <67472980+Ode42@users.noreply.github.com>
2020-07-13 20:07:05 +0300

    Bug fixed

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ f = open('data.json', 'r')
 data = json.load(f)
 
 print("****\n")
-print("Nimi: " + data[r]["Nimi"])
-print("Ikä: " + data[r]["Ikä"])
-print("Asuinpaikka: " + data[r]["Kaupunki"])
-print("Kuvaus: " + data[r]["Kuvaus"])
+print("Name: " + data[r]["Name"])
+# print all data you want to print
 
 f.close
